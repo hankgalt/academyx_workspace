2628e59c7f4fd00414086c8e6872082134a1dd7e
student <student@academyx.com>
2013-10-25 11:34:27 -0700

    Updated code to obtain, send and handle a message

--- PhotoGallery/src/com/academyx/android/photogallery/PhotoGalleryFragment.java
+++ PhotoGallery/src/com/academyx/android/photogallery/PhotoGalleryFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class PhotoGalleryFragment extends Fragment {
 	
@@ -18,23 +19,50 @@ public class PhotoGalleryFragment extends Fragment {
 	
 	GridView mGridView;
 	ArrayList<GalleryItem> mItems;
+	ThumbnailDownloader<ImageView> mThumbnailThread;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		Log.e(TAG, "onCreate() called ");
+		Log.i(TAG, "onCreate() called ");
 		super.onCreate(savedInstanceState);
 		this.setRetainInstance(true);
 		new FetchItemsTask().execute();
+		
+		mThumbnailThread = new ThumbnailDownloader<ImageView>();
+		mThumbnailThread.start();
+		mThumbnailThread.getLooper();
+		Log.i(TAG, "onCreate() - background thread started ");
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		Log.e(TAG, "onCreateView() called ");
+		Log.i(TAG, "onCreateView() called ");
 		
 		View v = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
 		
 		mGridView = (GridView) v.findViewById(R.id.gridView);
 		return v;
+	}
+	
+	@Override
+	public void onDestroy() {
+		Log.i(TAG, "onDestroy() called ");
+		super.onDestroy();
+		mThumbnailThread.quit();
+		Log.i(TAG, "onDestroy() - background thread destroyed ");
+		
+	}
+
+	public void setupAdapter() {
+		Log.i(TAG, "setupAdapter() called ");
+		
+		if (getActivity() == null || mGridView == null) return;
+		
+		if (mItems != null) {
+			mGridView.setAdapter(new GalleryItemAdapter(mItems));
+		} else {
+			mGridView.setAdapter(null);
+		}
 	}
 	
 	private class FetchItemsTask extends AsyncTask<Void,Void,ArrayList<GalleryItem>> {
@@ -48,20 +76,28 @@ public class PhotoGalleryFragment extends Fragment {
 			mItems = items;
 			setupAdapter();
 		}
-		
 	}
 	
-	public void setupAdapter() {
-		Log.e(TAG, "setupAdapter() called ");
-		
-		if (getActivity() == null || mGridView == null) {
-			return;
+	private class GalleryItemAdapter extends ArrayAdapter<GalleryItem> {
+		public GalleryItemAdapter(ArrayList<GalleryItem> items) {
+			super(getActivity(), 0, items);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.gallery_item, parent, false);
+			}
+			
+			ImageView imageView = (ImageView) convertView.findViewById(R.id.gallery_item_imageView);
+			imageView.setImageResource(R.drawable.brian_up_close);
+			
+			GalleryItem item = getItem(position);
+			mThumbnailThread.queueThumbnail(imageView, item.getUrl());
+			
+			return convertView;
 		}
 		
-		if (mItems != null) {
-			mGridView.setAdapter(new ArrayAdapter<GalleryItem>(getActivity(), android.R.layout.simple_gallery_item, mItems));
-		} else {
-			mGridView.setAdapter(null);
-		}
+		
 	}
 }
